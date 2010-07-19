a15c7347c77a82da4b04157ba69883f33b9b1bfb
James Luck <djluck20@hotmail.com>
2010-07-19 21:33:25 +0100

    Added the FixtureFetcher.py class- can parse fixtures for 2005 - 2010 for the 20 current premier league clubs

---
+++ Stats/FixtureFetcher.py
@@ -0,0 +1,153 @@
+import shelve
+import urllib
+from lxml import etree
+import re
+import calendar
+import datetime
+
+teams = [(359, 'Arsenal'),
+(362, 'Aston Villa'),
+(392, 'Birmingham City'),
+(365, 'Blackburn Rovers'),
+(358, 'Bolton Wanderers'),
+(346, 'Blackpool'),
+(363, 'Chelsea'),
+(368, 'Everton'),
+(370, 'Fulham'),
+(364, 'Liverpool'),
+(382, 'Manchester City'),
+(360, 'Manchester United'),
+(361, 'Newcastle United'),
+(336, 'Stoke City'),
+(366, 'Sunderland'),
+(367, 'Tottenham Hotspur'),
+(371, 'West Ham United'),
+(350, 'Wigan Athletic'),
+(383, 'West Bromwich Albion'),
+(380, 'Wolverhampton Wanderers')]
+
+
+class Fixture:
+    """
+    Represents a game played in the premier league.
+    """
+    
+    date_played = None
+    home_team = ""
+    away_team = ""
+    home_team_goals = 0  
+    away_team_goals = 0
+    league = ""
+    
+    def __str__ (self):
+        if self is not None:
+            return str(self.home_team) + "(" + str(self.home_team_goals) + ") vs " + str(self.away_team) + "(" + str(self.away_team_goals) + ") @ " + str(self.date_played) + " in the " + self.league
+        else:
+            return "(Empty)"
+
+
+class FixtureFetcher:
+    """
+    Retrieves a list of leauge fixtures (no cups/european tournaments) for the specified season.
+    Fixtures are parsed from the espn fixtures pages, where urls are of the form: 
+    
+    http://soccernet.espn.go.com/team/results?id=<team_id>&season=<season_year>
+    
+    where <team_id> is a positive integer and <season_year> is the opening year of a season 
+    (i.e. the season_year value for the 2009/2010 season would be 2009)
+    
+    Once all fixtures for a specified year have been succesfully parsed, they will be 'shelved'
+    """
+    
+    
+    def get_fixtures(self, year, use_local=True):
+        """
+        Gets all premier league fixtures for the specified year (year must be between 2005 & 2010).
+        If use_local is True, then if they exist, the shelved fixtures for the year will be returned 
+        without resorting to re-parsing the fixtures for each team.
+        """
+        
+        #preconditions
+        if year is None or year < 2005 or year > 2010:
+            raise Exception("for_year parameter must be a value between 2005 and 2010")
+        #get fixtures for year 
+        shelf = shelve.open("fixtures.shelf")
+        fixtures = None
+        if str(year) in shelf and use_local:
+            fixtures = shelf[str(year)]
+        else:
+            fixtures = self.__parse_fixtures(year)
+            shelf[str(year)] = fixtures
+            shelf.close()
+        return fixtures
+        
+    
+    def __parse_fixtures(self, year):
+        """ Gets all leauge fixtures for a specified year from the espn team fixture pages """
+        
+        fixtures = []
+        for team in teams:
+            team_id = team[0]
+            team_url = "http://soccernet.espn.go.com/team/results?id=" + str(team_id) + "&season=" + str(year) 
+            print "Parsing url: " + team_url
+            f = urllib.urlopen(team_url)
+            s = f.read()
+            fixtures.extend(self.__parse_fixture_page(s, year, team_id))
+        return fixtures
+            
+    
+    def __parse_fixture_page(self, html, year, team_id):
+        """ Parses the html of a espn fixture page, returning all league fixtures on the page """
+        
+        dom = etree.HTML(html)
+        rows = dom .xpath("//table[@class='tablehead']/tr")
+        
+        current_month = None
+        fixtures = []
+        for row in rows:
+            cells = row.xpath("./td")
+            if "class" in row.attrib and "row" in row.attrib["class"]:
+                #home_team_id = int(re.search("(?<=id=)[0-9]{2,4}", cells[2][0].attrib["href"]).group())
+                
+                competition = cells[6].text
+                is_english_leauge = re.search("league", competition, re.IGNORECASE) and re.search("english", competition, re.IGNORECASE)
+                status = cells[1].text
+                if is_english_leauge and status == None: 
+                    newFixture = Fixture()
+                    #day played cell, example value: "Sun. 18". Need to extract the day value.
+                    day = int(cells[0].text[5:])
+                    #if the game was played before june, it is in the later year part of the season
+                    newFixture.date_played = datetime.date(year if current_month > 6 else year + 1, current_month, day)
+                    #home team name cell (cell contains <a/> element which home team name)
+                    newFixture.home_team = cells[2][0].text
+                    #score cell, example value: 4-10. Must get home/away value (home score is leftmost value).
+                    score_m = re.search("(?P<home_score>[0-9]+)[^$]*(?P<away_score>[0-9]+)", cells[3].text)
+                    newFixture.home_team_goals = int(score_m.group("home_score"))
+                    newFixture.away_team_goals = int(score_m.group("away_score"))
+                    #away team name cell (cell contains <a/> element which home team name)
+                    newFixture.away_team = cells[4][0].text
+                    newFixture.league = competition
+                    fixtures.append(newFixture)
+            elif "class" in row.attrib and row.attrib["class"] == "colhead":
+                #month/year column data, example value = "Aug. '09". Must fetch the month abreviation and convert that into the numeric month value.
+                monthAbbr = cells[0].text[0:3]
+                current_month = list(calendar.month_abbr).index(monthAbbr)
+                
+        return fixtures
+                
+fp = FixtureFetcher()
+fixtures = fp.get_fixtures(2005,False)
+print "There are %(count)d fixtures" % {"count" : len(fixtures)}
+fixtures = sorted(fixtures, lambda x, y: cmp(x.home_team, y.home_team))
+i = 0
+team_fixture_count = dict( [(f.home_team, 0) for f in fixtures])
+print team_fixture_count
+for f in fixtures:
+    print "#" + str(i) + " : " + str(f)
+    team_fixture_count[f.home_team] =  team_fixture_count[f.home_team] + 1
+    i += 1
+    
+print team_fixture_count
+
+
+    
--- Stats/statsfetcher.py
+++ Stats/statsfetcher.py
@@ -3,6 +3,28 @@ import sgmllib
 import re
 import calendar
 import datetime
+from xml.dom.minidom import parse, parseString
+from lxml import etree
+
+class Fixture:
+    date_played = None
+    home_team = ""
+    away_team = ""
+    home_team_goals = 0  
+    away_team_goals = 0
+    
+    def __str__ (self):
+        if self is not None:
+            return str(self.home_team) + "(" + str(self.home_team_goals) + ") vs " + str(self.away_team) + "(" + str(self.away_team_goals) + ") @ " + str(self.date_played)
+        else:
+            return "(Empty)"
+    
+class Player:
+    name = ""
+    fixtures_played = []
+    
+    
+    
 
 class PlayerParser(sgmllib.SGMLParser):
     "An html parser that can extract player statistics from soccernet.espn.go.com gamelog pages."
@@ -15,54 +37,68 @@ class PlayerParser(sgmllib.SGMLParser):
     def __init__(self, verbose=0):
         sgmllib.SGMLParser.__init__(self, verbose)
         
-class FixtureParser(sgmllib.SGMLParser):
-    "An html parser that can extract fixture information from soccernet.espn.go.com fixture pages."
+#class FixtureParser(sgmllib.SGMLParser):
+    #"An html parser that can extract fixture information from soccernet.espn.go.com fixture pages."
     
-    def parse(self, s, year):
-        "Parse the given string 's'."
-        self.year = year
-        self.feed(s)
-        self.close()
+    
+    #def parse(self, s, year):
+        #"Parse the given string 's'."
+        #self.year = year
+        #self.feed(s)
+        #self.close()
         
-
-    def __init__(self, verbose=0):
-        sgmllib.SGMLParser.__init__(self, verbose)
-        self.inData = False
-        self.inMonthHeader = False
-        self.fixtureResults = []
-        self.pos = 0
-        self.currentMonth = None
-        self.year = 0
+    #def __init__(self, verbose=0):
+        #sgmllib.SGMLParser.__init__(self, verbose)
+        #self.inData = False
+        #self.inMonthHeader = False
+        #self.fixtureResults = []
+        #self.tdIndex = 0
+        #self.currentMonth = None
+        #self.year = 0
+        #self.currentFixture = None
         
-    def start_tr(self, attributes):
-        self.pos = 0
-        self.inMonthHeader = False
-        self.inData = False
-        for key, value in attributes:
-            if key == "class" and value == "colhead":
-                self.inMonthHeader = True
-            elif key == "class" and "row" in value:
-                self.inData = True
+    #def start_tr(self, attributes):
+        #self.tdIndex = 0
+        #self.inMonthHeader = False
+        #self.inData = False
+        #for key, value in attributes:
+            #if key == "class" and value == "colhead":
+                #self.inMonthHeader = True
+            #elif key == "class" and "row" in value:
+                #self.inData = True
         
-    def start_td(self, attributes):
-        self.pos += 1
+    #def start_td(self, attributes):
+        #self.tdIndex += 1
         
-    def end_tr(self):
-        self.pos = 0
+    #def end_tr(self):
+        #self.tdIndex = 0
             
-    def handle_data(self, data):
-        if self.inMonthHeader:
-            monthAbbr = data[0:3]
-            self.currentMonth =  list(calendar.month_abbr).index(monthAbbr)
-            self.inMonthHeader = False
-        elif self.inData:
-            if self.pos == 1:
-                #date column data. date example value: 'Sun. 18'
-                month = int(data[5:])
-                self.fixtureResults.append((datetime.date(self.year, self.currentMonth, month)))
+    #def handle_data(self, data):
+        #if data == "\n": #ignore blank data
+            #return
+        #if self.inMonthHeader:
+            ##month/year column data, example value = "Aug. '09". Must fetch the month abreviation and convert that into the numeric month value.
+            #monthAbbr = data[0:3]
+            #self.currentMonth =  list(calendar.month_abbr).index(monthAbbr)
+            #self.inMonthHeader = False
+        #elif self.inData:
+            #print "handle called (in data)"
+            #if self.tdIndex == 1:
+                ##date column data, example value: "Sun. 18". Need to extract the day value.
+                #day = int(data[5:])
+                #self.currentFixture = Fixture()
+                #self.currentFixture.date_played = datetime.date(self.year, self.currentMonth, day)
+            #elif self.tdIndex == 3:
+                ##home team name (get entire string)
+                #self.currentFixture.home_team =  data
+            ##elif self.tdIndex == 4:
+                ###score column data, example value: 4-10. Must get home/away value (home score is leftmost value).
+                ##scoreM = re.match("(P<homeScore>[0-9]+) - (P<awayScore>[0-9]+)", data)
+                ##self.currentFixture.homeScore = int(scoreM.group("homeScore"))
+                ##self.currentFixture.awayScore = int(scoreM.group("awayScore"))
 
-    def get_fixture_results(self):
-        return self.fixtureResults
+    #def get_fixture_results(self):
+        #return self.fixtureResults
         
 class TeamParser(sgmllib.SGMLParser):
     "An html parser that can extract lists of players names/ids from soccernet.espn.go.com club pages."
@@ -127,9 +163,71 @@ class TeamParser(sgmllib.SGMLParser):
     #print teamName + ":" 
     #print teamParser.get_players()
     
-f = urllib.urlopen("http://soccernet.espn.go.com/team/results?id=359&season=2009&cc=5739&_league=all")
+#f = urllib.urlopen("http://soccernet.espn.go.com/team/results?id=359&season=2009&cc=5739&_league=all")
+#s = f.read()
+#fp = FixtureParser()
+##fp.parse(s, 2009)
+
+#html = etree.HTML(s)
+#rows = html.xpath("//table[@class='tablehead']/tr")
+
+#current_month = None
+#fixtures = []
+#for row in rows:
+    #cells = row.xpath("./td")
+    #i=0
+    #if "class" in row.attrib and "row" in row.attrib["class"]:
+        #if re.search("premier league", cells[6].text, re.IGNORECASE): 
+            #newFixture = Fixture()
+            ##day played cell, example value: "Sun. 18". Need to extract the day value.
+            #day = int(cells[0].text[5:])
+            #newFixture.date_played = datetime.date(2009, current_month, day)
+            ##home team name cell (cell contains <a/> element which home team name)
+            #newFixture.home_team = cells[2][0].text
+            ##score cell, example value: 4-10. Must get home/away value (home score is leftmost value).
+            #score_m = re.search("(?P<home_score>[0-9]+)[^$]*(?P<away_score>[0-9]+)", cells[3].text)
+            #newFixture.home_team_goals = int(score_m.group("home_score"))
+            #newFixture.away_team_goals = int(score_m.group("away_score"))
+            ##away team name cell (cell contains <a/> element which home team name)
+            #newFixture.away_team = cells[4][0].text
+            #fixtures.append(newFixture)
+    #elif "class" in row.attrib and row.attrib["class"] == "colhead":
+        ##month/year column data, example value = "Aug. '09". Must fetch the month abreviation and convert that into the numeric month value.
+        #monthAbbr = cells[0].text[0:3]
+        #current_month = list(calendar.month_abbr).index(monthAbbr)
+#print "There are " + str(len(fixtures)) + " fixtures"
+#for f in fixtures:
+    #print f
+    
+f = urllib.urlopen("http://soccernet.espn.go.com/players/gamelog?id=14582&season=2007&cc=5739")
 s = f.read()
 fp = FixtureParser()
-fp.parse(s, 2009)
+#fp.parse(s, 2009)
 
 
+
+html = etree.HTML(s)
+rows = html.xpath("//table[@class='tablehead']/tr")
+
+players = []
+for row in rows:
+    cells = row.xpath("./td")
+    i=0
+    if "class" in row.attrib and "row" in row.attrib["class"]:
+        if re.search("premier league", cells[3].text, re.IGNORECASE): 
+            newFixture = Fixture()
+            #day played cell, example value: "Sun. 18". Need to extract the day value.
+            day = int(cells[0].text[5:])
+            newFixture.date_played = datetime.date(2009, current_month, day)
+            #home team name cell (cell contains <a/> element which home team name)
+            newFixture.home_team = cells[2][0].text
+            #score cell, example value: 4-10. Must get home/away value (home score is leftmost value).
+            score_m = re.search("(?P<home_score>[0-9]+)[^$]*(?P<away_score>[0-9]+)", cells[3].text)
+            newFixture.home_team_goals = int(score_m.group("home_score"))
+            newFixture.away_team_goals = int(score_m.group("away_score"))
+            #away team name cell (cell contains <a/> element which home team name)
+            newFixture.away_team = cells[4][0].text
+            fixtures.append(newFixture)
+print "There are " + str(len(fixtures)) + " fixtures"
+for f in fixtures:
+    print f
